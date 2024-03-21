1a0a3800be7b5a79443d4ae490b619186b294aad
KedarPanchal <kedar.t.panchal@outlook.com>
2024-03-20 22:08:39 -0500

    Made creating MMS emails more efficient

--- src/main/java/com/kpanchal/ttp/mailtools/Contact.java
+++ src/main/java/com/kpanchal/ttp/mailtools/Contact.java
@@ -1,39 +1,11 @@
 package com.kpanchal.ttp.mailtools;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.LinkedHashMap;
-
 public class Contact {
 
-    private static final Map<String, String> providers = Contact.createMap();
+    private static final String[] providers = {"mms.alltelwireless.com", "mms.att.net", "myboostmobile.com", "mailmymobile.net", "mms.cricketwireless.net", "msg.fi.google.com", "mymetropcs.com", "mypixmessages.com", "pm.sprint.com", "tmomail.net", "mms.uscc.net", "vwpix.com", "vmpix.com", "mypixmessages.com"};
 
     public static String createContact(String number, int carrierIndex) {
-        return number + "@" + providers.get(Contact.providers.keySet().toArray()[carrierIndex]);
-    }
-
-    private static Map<String, String> createMap() {
-        Map<String, String> ret = new LinkedHashMap<>();
-        ret.put("Alltel", "mms.alltelwireless.com");
-        ret.put("AT&T", "mms.att.net");
-        ret.put("Boost Mobile", "myboostmobile.com");
-        ret.put("Consumer Cellular", "mailmymobile.net");
-        ret.put("Cricket Wirless", "mms.cricketwireless.net");
-        ret.put("Google Fi Wireless", "msg.fi.google.com");
-        ret.put("MetroPCS", "mymetropcs.com");
-        ret.put("Spectrum Mobile", "mypixmessages.com");
-        ret.put("Sprint", "pm.sprint.com");
-        ret.put("T-Mobile", "tmomail.net");
-        ret.put("U.S. Cellular", "mms.uscc.net");
-        ret.put("Verizon Wireless", "vwpix.com");
-        ret.put("Virgin Mobile", "vmpix.com");
-        ret.put("XFinity Mobile", "mypixmessages.com");
-        
-        return Collections.unmodifiableMap(ret);
-    }
-
-    public static int getNumberOfCarriers() {
-        return Contact.providers.size();
+        return number + "@" + providers[carrierIndex];
     }
 
     public static String parseNumber(String email) {
